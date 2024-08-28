f676deae061282182021e63ed9745e1cd1f46cfe
txstc55 <txstc55@gmail.com>
2024-08-28 15:47:30 -0700

    add test file

--- tests/symbolic/test_symbolic.py
+++ tests/symbolic/test_symbolic.py
@@ -2,5 +2,6 @@ from yasps import symbolic
 
 x = symbolic('x')
 y = symbolic('y')
-z = symbolic(y)
-print(-x + y + 1 - z)
+z = -symbolic(y)
+print(-x + y + 1 - symbolic.sin(z))
+print(symbolic.select(x, y, z))
