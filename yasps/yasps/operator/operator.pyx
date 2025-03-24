26ba505ecac5a6a7f0c673694b615dab03a72764
txstc55 <txstc55@gmail.com>
2025-03-24 14:26:31 -0700

    turn operator into cython

--- yasps/yasps/operator/operator.pyx
+++ yasps/yasps/operator/operator.pyx
@@ -1,37 +1,40 @@
 # cython: language_level=3
-# operator is used to define what property
-# an operator should have
-# and how the string should be generated
-# from the operator and its children
-class operator:
-  def __init__(self, name: str, operator_type: int, commutative: bool):
-    self.__name = name
-    self.__operator_type = operator_type
-    self.__commutative = commutative
+
+cdef class operator:
+  cdef readonly str _name
+  cdef readonly int _operator_type
+  cdef readonly bint _commutative
+
+  def __init__(self, str name, int operator_type, bint commutative):
+    self._name = name
+    self._operator_type = operator_type
+    self._commutative = commutative
 
   @property
-  def name(self)->str:
-    return self.__name
+  def name(self) -> str:
+    return self._name
 
   @property
-  def type(self)->int:
-    # 0 for op(x)
-    # 1 for x op y
-    # 2 for op($0, $1, ...)
-    # 3 for other things
-    return self.__operator_type
-
+  def type(self) -> int:
+    """0: op(x), 1: x op y, 2: op($0, $1, ...), 3: other"""
+    return self._operator_type
 
   @property
-  def commutative(self)->bool:
-    return self.__commutative
+  def commutative(self) -> bint:
+    return self._commutative
 
+  def __richcmp__(self, other, int op):
+    if op == 2:  # == operator
+      return self._is_equal(other)
+    elif op == 3:  # != operator
+      return not self._is_equal(other)
+    else:
+      return NotImplemented
 
-  def __str__(self)->str:
-    return f"operator({self.__name}, type={self.type}, commutative={self.commutative})"
+  cdef bint _is_equal(self, operator other):
+    return (self._name == other._name and
+            self._operator_type == other._operator_type and
+            self._commutative == other._commutative)
 
-  # check if two operators are equal
-  def __eq__(self, other)->bool:
-    if self.name == other.name and self.type == other.type and self.commutative == other.commutative:
-      return True
-    return False
+  def __str__(self) -> str:
+    return f"operator({self.name}, type={self.type}, commutative={self.commutative})"
