778258948657dcca36f105c169fcd697616d4596
txstc55 <txstc55@gmail.com>
2025-05-02 17:27:04 -0700

    changed file

--- yasps/install.sh
+++ yasps/install.sh
@@ -4,5 +4,5 @@ python setup.py bdist_wheel
 pip install dist/*.whl --force-reinstall
 # pip install .
 
-$fcm_token=$(cat fcm_token)
+fcm_token=$(cat fcm_token)
 gmtu --fcm-token $fcm_token --content "YASPS compile finished. GET THE FUCK BACK TO WORK!"
