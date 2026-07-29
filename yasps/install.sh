352e61c3f21a75cc3b4a18a3f58c6c049cd6031d
txstc55 <txstc55@gmail.com>
2026-07-28 19:02:33 -0700

    remove gmtu push

--- yasps/install.sh
+++ yasps/install.sh
@@ -4,5 +4,5 @@ python setup.py bdist_wheel
 python -m pip install dist/*.whl --force-reinstall --no-deps
 # pip install .
 
-fcm_token=$(cat fcm_token)
-gmtu --fcm-token $fcm_token --content "YASPS compile finished. GET THE FUCK BACK TO WORK!"
+# fcm_token=$(cat fcm_token)
+# gmtu --fcm-token $fcm_token --content "YASPS compile finished. GET THE FUCK BACK TO WORK!"
